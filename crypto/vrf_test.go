7e8cb794ecadaaa4512dfe10449382332e015b8b
hongsup.so <hongsup.so@linecorp.com>
2020-01-06 21:18:13 +0900

    Add more unittests.

--- crypto/vrf_test.go
+++ crypto/vrf_test.go
@@ -1,9 +1,35 @@
 package crypto
 
-import "testing"
+import (
+	"bytes"
+	"encoding/hex"
+	"testing"
+)
+
+func toString(s []uint8) string {
+	return hex.EncodeToString(s)
+}
 
 func TestVrfKeygen(t *testing.T) {
-	//var pk, sk = VrfKeygen()
-	var pk, sk = "pk", "sk"
-	t.Logf("private key: %s, public key: %s", pk, sk)
+	var pk, sk = VrfKeygen()
+	//var pk, sk = "pk", "sk"
+	t.Logf("private key: %s, public key: %s", toString(sk[:]), toString(pk[:]))
+}
+
+func TestVrfPrivkey_Prove(t *testing.T) {
+	var pk, sk = VrfKeygen()
+	var message = []byte("This is test message.")
+	t.Logf("private key: %s, public key: %s", toString(sk[:]), toString(pk[:]))
+	var proof, ok = sk.Prove(message)
+	t.Logf("proof: %s, ok: %t", toString(proof[:]), ok)
+
+	var output, ok2 = proof.Hash()
+	t.Logf("output : %s, ok: %t", toString(output[:]), ok2)
+
+	var ok3, output2 = pk.Verify(proof, message)
+	t.Logf("output2: %s, ok: %t", toString(output2[:]), ok3)
+
+	if bytes.Compare(output[:], output2[:]) != 0 {
+		t.Error("output not matches")
+	}
 }
--- main.go
+++ main.go
@@ -1,12 +1,19 @@
-package main	// import "github.com/zemyblue/vrf-go"
+package main // import "github.com/zemyblue/vrf-go"
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+)
 import (
 	vrf "vrf-go/crypto"
 )
 
-func main()  {
+func toHexString(str []byte) string {
+	return hex.EncodeToString(str)
+}
+
+func main() {
 	fmt.Println("Hello")
 	var pk, sk = vrf.VrfKeygen()
-	fmt.Printf("private key: %s, public key: %s\n", pk, sk)
+	fmt.Printf("private key: %s, public key: %s \n", toHexString(pk[:]), toHexString(sk[:]))
 }
